753b019f1b3f19fc36e01baa4b8787092171aa2f
ValiathMain <frankplace3210@gmail.com>
2024-08-23 15:21:16 -0600

    Add files via upload

--- Dia 7- sets.py
+++ Dia 7- sets.py
@@ -18,3 +18,27 @@ print(mi_otro_set) # un set no es una estrutura ordenada
 mi_otro_set.add("Brenda") # En los sets los datos no se pueden repetir
 
 print(mi_otro_set)
+
+print("Valianth" in mi_otro_set) # en lugar de imprimir el dato con el print lo retorna como un dato boolean true o false
+
+mi_otro_set.remove("Valianth")
+print(mi_otro_set)
+
+mi_otro_set.clear()
+print(len(mi_otro_set))
+
+del mi_otro_set
+# print(mi_otro_set)
+
+mi_set = {"Alejandro","Valianth",26}
+mi_lista = list(mi_set)
+
+print(mi_lista)
+print(mi_lista[1]) # hacer este tipo de transformaciones no es lo mas recomendado ya que de todos modos no llega a tener un orden
+
+mi_otro_set = {"Python","C++","Java"}
+
+nuevo_set = mi_set.union(mi_otro_set)
+print(nuevo_set.union(nuevo_set).union(mi_set).union({"Go"})) # no acepta repetidos ||||| si no se pone asi .union({"Go"}) agrega las letras por separado
+
+print(nuevo_set.difference(mi_set))
